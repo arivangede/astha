29c36d882e309d584a5ac73048b10f6b9186c46c
Arivan G. Eka Satria <arivan.gede@gmail.com>
2023-08-15 10:04:43 +0800

    commit pertama

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Astha Web Devs</title>
+</head>
+
+<body>
+    <nav>
+        <div class="logo">
+            <h3>ASTHA</h3>
+            <div class="moon">
+                <div class="circle1"></div>
+                <div class="circle2"></div>
+            </div>
+        </div>
+
+        <div class="nav-links">
+            <a href="#home">Home</a>
+            <a href="">Services</a>
+            <a href="">Portfolio</a>
+            <a href="">About Us</a>
+            <a href="">Contact</a>
+        </div>
+    </nav>
+
+    <section id="home">
+        <div class="content">
+            <h1>Selamat Datang di ASTHA WebDevs!</h1>
+            <p>Menghadirkan <span>Keindahan</span> dan <span>Kualitas</span> di Dunia Digital.</p>
+        </div>
+        <button>Jelajahi Layanan Kami</button>
+    </section>
+
+</body>
+
+</html>
